18c421a842ab601fc41846ff21dd7b6845f65c46
benjaquintana <elbenjaq@gmail.com>
2021-01-12 17:42:01 +0000

    1

---
+++ calendario.php
@@ -0,0 +1,43 @@
+<!--Header-->
+<?php include_once 'includes/templates/header.php'?>
+<!--Fin Header-->
+
+  <section class="seccion contenedor">
+    <h2>Calendario de Eventos</h2>
+
+        <?php
+            try {
+                require_once('includes/funciones/db_conexion.php');
+                $sql = " SELECT evento_id, nombre_evento, fecha_evento, hora_evento, cat_evento, nombre_invitado, apellido_invitado ";
+                $sql .= " FROM eventos ";
+                $sql .= " INNER JOIN categoria_evento ";
+                $sql .= " ON eventos.id_cat_evento = categoria_evento.id_categoria ";
+                $sql .= " INNER JOIN invitados ";
+                $sql .= " ON eventos.id_inv = invitados.invitado_id ";
+                $sql .= " ORDER BY evento_id ";
+                $resultado = $conn->query($sql);
+            } catch (\Exception $e) {
+                echo $e->getMessage();
+            }
+        ?>
+
+        <div class="calendario">
+            <?php
+                $calendario = array();
+                while( $eventos = $resultado->fetch_assoc() ) { ?>
+                  <pre>
+                    <?php var_dump($eventos); ?>
+                  </pre>
+                <?php } ?>
+
+        </div>
+
+        <?php
+            $conn->close();
+        ?>
+
+  </section>
+
+<!--Footer-->
+<?php include_once 'includes/templates/footer.php'?>
+<!--Fin Footer-->
---
+++ includes/funciones/db_conexion.php
@@ -0,0 +1,6 @@
+<?php
+  $conn = new mysqli('sql3.freesqldatabase.com', 'sql3385031', '8fCnKSl8Ct', 'sql3385031');
+  if ($conn->connect_error) {
+    echo $error -> $conn->connect_error;
+  }
+?>
