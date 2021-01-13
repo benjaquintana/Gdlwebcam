d49acb89cb16578cf451babda5bc013964a290ed
benjaquintana <elbenjaq@gmail.com>
2021-01-13 18:03:54 +0000

    1

--- calendario.php
+++ calendario.php
@@ -8,13 +8,13 @@
         <?php
             try {
                 require_once('includes/funciones/db_conexion.php');
-                $sql = " SELECT evento_id, nombre_evento, fecha_evento, hora_evento, cat_evento, nombre_invitado, apellido_invitado ";
+                $sql = " SELECT id_evento, nombre_evento, fecha_evento, hora_evento, cat_evento, icono, nombre_invitado, apellido_invitado ";
                 $sql .= " FROM eventos ";
                 $sql .= " INNER JOIN categoria_evento ";
                 $sql .= " ON eventos.id_cat_evento = categoria_evento.id_categoria ";
                 $sql .= " INNER JOIN invitados ";
-                $sql .= " ON eventos.id_inv = invitados.invitado_id ";
-                $sql .= " ORDER BY evento_id ";
+                $sql .= " ON eventos.id_inv = invitados.id_invitado ";
+                $sql .= " ORDER BY id_evento ";
                 $resultado = $conn->query($sql);
             } catch (\Exception $e) {
                 echo $e->getMessage();
@@ -24,14 +24,54 @@
         <div class="calendario">
             <?php
                 $calendario = array();
-                while( $eventos = $resultado->fetch_assoc() ) { ?>
-                  <pre>
-                    <?php var_dump($eventos); ?>
-                  </pre>
+                while( $eventos = $resultado->fetch_assoc() ) { 
+                    $fecha = $eventos['fecha_evento'];
+                    $evento = array(
+                        'titulo' => $eventos['nombre_evento'],
+                        'fecha' => $eventos['fecha_evento'],
+                        'hora' => $eventos['hora_evento'],
+                        'categoria' => $eventos['cat_evento'],
+                        'icono' => $eventos['icono'],
+                        'invitado' => $eventos['nombre_invitado'] . " " . $eventos['apellido_invitado']
+                    );
+                    $calendario[$fecha] [] = $evento; ?>
                 <?php } ?>
 
-        </div>
+                <?php 
+                    //Imprime todos los eventos
+                    foreach ($calendario as $dia => $lista_eventos) { ?>
+                        <h3>
+                            <i class="fa fa-calendar"></i>
+                            <?php 
+                                //Unix
+                                setlocale(LC_TIME, 'es_ES.UTF-8');
+                                //Windows
+                                setlocale(LC_TIME, 'spanish');
+                                
+                                echo strftime("%A, %d de %B del %Y", strtotime($dia) ); ?>
+                        </h3>
 
+                    <?php foreach ($lista_eventos as $evento) { ?>
+                        
+                        <div class="dia">
+                            <p class="titulo"><?php echo $evento['titulo']; ?></p>
+                            <p class="hora">
+                                <i class="fa fa-clock-o" aria-hidden="true"></i>
+                                <?php echo $evento['fecha'] . " " . $evento['hora']; ?>
+                            </p>
+                            <p>
+                                <i class="fa <?php echo $evento['icono']; ?>" aria-hidden="true"></i>
+                                <?php echo $evento['categoria']; ?>
+                            </p>
+                            <p>
+                                <i class="fa fa-user" aria-hidden="true"></i>
+                                <?php echo $evento['invitado']; ?>
+                            </p>
+                        </div>
+
+                    <?php } //Fin foreach eventos ?> 
+                   <?php } //Fin foreach dias ?> 
+        </div> <!--Calendario-->
         <?php
             $conn->close();
         ?>
--- includes/funciones/db_conexion.php
+++ includes/funciones/db_conexion.php
@@ -1,5 +1,5 @@
 <?php
-  $conn = new mysqli('sql3.freesqldatabase.com', 'sql3385031', '8fCnKSl8Ct', 'sql3385031');
+  $conn = new mysqli('localhost', 'root', '', 'gdlwebcamp');
   if ($conn->connect_error) {
     echo $error -> $conn->connect_error;
   }
