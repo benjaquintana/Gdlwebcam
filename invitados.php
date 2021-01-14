0d595effbbc591f31c90a4abff828bd843301adc
benjaquintana <elbenjaq@gmail.com>
2021-01-14 22:11:40 +0000

    1

--- invitados.php
+++ invitados.php
@@ -4,32 +4,31 @@
 
   <section class="seccion contenedor">
     <h2>Invitados</h2>
-        <?php
-            try {
-                require_once('includes/funciones/db_conexion.php');
-                $sql = " SELECT id_evento, nombre_evento, fecha_evento, hora_evento, cat_evento, icono, nombre_invitado, apellido_invitado ";
-                $sql .= " FROM eventos ";
-                $sql .= " INNER JOIN categoria_evento ";
-                $sql .= " ON eventos.id_cat_evento = categoria_evento.id_categoria ";
-                $sql .= " INNER JOIN invitados ";
-                $sql .= " ON eventos.id_inv = invitados.id_invitado ";
-                $sql .= " ORDER BY id_evento ";
-                $resultado = $conn->query($sql);
-            } catch (\Exception $e) {
-                echo $e->getMessage();
-            }
-        ?>
+      <?php
+          try {
+              require_once('includes/funciones/db_conexion.php');
+              $sql = " SELECT * FROM invitados ";
+              $resultado = $conn->query($sql);
+          } catch (\Exception $e) {
+              echo $e->getMessage();
+          }
+      ?>
 
-        <div class="calendario">
-            <?php
-                $calendario = array();
-                while( $eventos = $resultado->fetch_assoc() ) {
-
-                };
-            ?>
-        <?php
-            $conn->close();
-        ?>
+      <section class="invitados contenedor seccion">
+        <ul class="lista_invitados clearfix">
+          <?php while($invitados = $resultado->fetch_assoc() ) { ?>
+            <li>
+              <div class="invitado">
+                <img src="img/<?php echo $invitados['url_imagen'] ?>" alt="imagen_invitado">
+                <p><?php echo $invitados['nombre_invitado'] . " " . $invitados['apellido_invitado'] ?></p>
+              </div>
+            </li>
+          <?php } ?>
+        </ul>
+      </section>
+      <?php
+          $conn->close();
+      ?>
 
   </section>
 
