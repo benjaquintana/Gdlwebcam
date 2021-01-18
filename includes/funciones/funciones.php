e053af5241fb350d77c1f160e44a5c17227af628
benjaquintana <elbenjaq@gmail.com>
2021-01-18 20:04:58 +0000

    1

---
+++ includes/funciones/funciones.php
@@ -0,0 +1,7 @@
+<?php
+  function productos_json(&$boletos, &$camisas = 0, &$etiquetas = 0){
+    echo "hola desde producto json";
+  }
+
+
+?>
--- validar_registro.php
+++ validar_registro.php
@@ -4,8 +4,27 @@
 <section class="seccion contenedor">
     <h2>Resumen Registro</h2>
 
-  <pre><?php var_dump($_POST) ?></pre>
-</section>
+    <?php if(isset($_POST['submit'])):
+    $nombre = $_POST['nombre'];
+    $apellido = $_POST['apellido'];
+    $email = $_POST['email'];
+    $fecha = date('Y-m-d H:i:s');
+    $regalo = $_POST['regalo'];
+    $total = $_POST['total_pedido'];
+
+    //Pedidos
+    $boletos = $_POST['boletos'];
+    $camisas = $_POST['pedido_camisas'];
+    $etiquetas = $_POST['pedido_etiquetas'];
+    include_once 'includes/funciones/funciones.php';
+    echo productos_json($boletos, $camisas, $etiquetas);
+
+    ?>
+
+    <pre><?php var_dump($_POST) ?></pre>
+
+    <?php endif; ?>
+  </section>
 <!--Footer-->
 <?php include_once 'includes/templates/footer.php'?>
 <!--Fin Footer-->
