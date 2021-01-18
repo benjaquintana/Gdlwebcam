f0f4180e30dea051c19bc1a0808d4fe686affc77
benjaquintana <elbenjaq@gmail.com>
2021-01-18 23:18:51 +0000

    1

--- validar_registro.php
+++ validar_registro.php
@@ -1,30 +1,46 @@
-<!--Header-->
-<?php include_once 'includes/templates/header.php'?>
-<!--Fin Header-->
-<section class="seccion contenedor">
-    <h2>Resumen Registro</h2>
-
-    <?php if(isset($_POST['submit'])):
+<?php if(isset($_POST['submit'])):
     $nombre = $_POST['nombre'];
     $apellido = $_POST['apellido'];
     $email = $_POST['email'];
     $fecha = date('Y-m-d H:i:s');
     $regalo = $_POST['regalo'];
     $total = $_POST['total_pedido'];
-
     //Pedidos
     $boletos = $_POST['boletos'];
     $camisas = $_POST['pedido_camisas'];
     $etiquetas = $_POST['pedido_etiquetas'];
     include_once 'includes/funciones/funciones.php';
-    echo productos_json($boletos, $camisas, $etiquetas);
-
+    $pedido = productos_json($boletos, $camisas, $etiquetas);
+    //Eventos
+    $eventos = $_POST['registro'];
+    $registro = eventos_json($eventos);
+    try {
+      require_once('includes/funciones/db_conexion.php');
+      $stmt = $conn->prepare("INSERT INTO registrados (nombre_registrado, apellido_registrado, email_registrado, fecha_registro, pases_articulos, talleres_registrados, regalo, total_pagado) VALUES (?,?,?,?,?,?,?,?)");
+      $stmt->bind_param("ssssssis", $nombre, $apellido, $email, $fecha, $pedido, $registro, $regalo, $total);
+      $stmt->execute();
+      $stmt->close();
+      $conn->close();
+      header('Location: validar_registro.php?exitoso=1');
+    } catch (\Exception $e) {
+      echo $e->getMessage();
+    }
     ?>
+<?php endif; ?>
+<!--Header-->
+<?php include_once 'includes/templates/header.php'?>
+<!--Fin Header-->
+<section class="seccion contenedor">
+    <h2>Resumen Registro</h2>
 
-    <pre><?php var_dump($_POST) ?></pre>
+    <?php if(isset($_GET['exitoso'])):
+      if($_GET['exitoso'] == "1"):
+        echo "Registro Exitoso";
+      endif;
+     endif; ?>
 
-    <?php endif; ?>
   </section>
+
 <!--Footer-->
 <?php include_once 'includes/templates/footer.php'?>
 <!--Fin Footer-->
