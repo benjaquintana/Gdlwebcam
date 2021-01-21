dd32ef7433201417012a84d58c888aa6d748783b
benjaquintana <elbenjaq@gmail.com>
2021-01-21 15:50:59 +0000

    1

--- includes/paypal.php
+++ includes/paypal.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 
 //url aquispe
-define('URL_SITIO', '');
+define('URL_SITIO', 'https://3000-fa6af479-158e-41b8-9d15-57c79cd4ad31.ws-us03.gitpod.io');
 
 require 'paypal/autoload.php';
 
